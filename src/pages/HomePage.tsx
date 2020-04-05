a3d1f161f6a65dfedc1ebe01da06c5b903da595a
yurilee9696 <yurilee9696@gmail.com>
2020-04-05 14:58:41 +0900

    MOD HomePage modal component 뜨도록 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
-
 import Button from '../components/button';
+import Modal from '../components/organisms/modal';
+import useModal from '../hooks/useModal';
 
 const HomePage: React.FC<RouteComponentProps> = (props) => {
   function searchWord() {
     const url = 'search';
     props.history.push(url);
   }
+
+  const { onToggleModal} = useModal();
   return (
     <>
       <form>
         <input />
         <Button onClick={searchWord}>검색</Button>
       </form>
+      <Button onClick={onToggleModal}>필터링</Button>
+      <Modal></Modal>
     </>
   );
 };
